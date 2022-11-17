cdcedebb746f5e5ece1a215e68fcf11732199fc3
collegewise-result <shobd35@gmail.com>
2022-11-17 21:01:32 +0600

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -6,30 +6,16 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.base import MIMEBase
 from email import encoders
 import urllib.request 
-# importing required modules 
-import PyPDF2 
+from tika import parser # pip install tika
+
 
 
 url = "https://rupalibank.com.bd/admin_web/files/currency/CurrencyDetail.pdf"
 urllib.request.urlretrieve(url, "CurrencyDetail.pdf")
 
-# creating a pdf file object 
-pdfFileObj = open('CurrencyDetail.pdf', 'rb') 
-    
-# creating a pdf reader object 
-pdfReader = PyPDF2.PdfFileReader(pdfFileObj) 
-    
-# printing number of pages in pdf file 
-print(pdfReader.numPages) 
-    
-# creating a page object 
-pageObj = pdfReader.getPage(0) 
-    
-# extracting text from page 
-print(pageObj.extractText()) 
-    
-# closing the pdf file object 
-pdfFileObj.close() 
+raw = parser.from_file('CurrencyDetail.pdf')
+print(raw['content'])
+
 
 
 '''
