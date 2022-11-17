697325d094f818d654e26c3767ecf75903bdcd44
collegewise-result <shobd35@gmail.com>
2022-11-17 19:49:37 +0600

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -3,9 +3,10 @@ import os
 
 
 port = 465
-smtp_server = "smtp.gmail.com"
+smtp_server = "smtp.yandex.com"
 USERNAME = os.environ.get('USER_EMAIL')
 PASSWORD = os.environ.get('USER_PASSWORD')
+RECEIPIENT = os.environ.get('USER_RECEIPIENT')
 message = """\
 Subject: GitHub Email Report
 
@@ -15,4 +16,4 @@ This is your daily email report.
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
     server.login(USERNAME,PASSWORD)
-    server.sendmail(USERNAME,USERNAME,message)
+    server.sendmail(USERNAME,RECEIPIENT,message)
